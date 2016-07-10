14ef2e9ee81346a3647bcf6f9cfd8627cb941d3f
えばたあや <ayaaaaa.0u0w@gmail.com>
2016-07-11 00:33:27 +0900

    add count down timer

--- app/src/main/java/com/example/aya/nantokaapp/TimerActivity.java
+++ app/src/main/java/com/example/aya/nantokaapp/TimerActivity.java
@@ -2,15 +2,26 @@ package com.example.aya.nantokaapp;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 /**
  * Created by aya on 2016/05/05.
  */
 public class TimerActivity extends AppCompatActivity {
+
+
+    private static final int DEFAULT_START_TIME_MINUTE = 30;
+    private static final int DEFAULT_START_TIME_SECOND = 0;
+    private static final int ONE_SECOND = 60000;
+    private static final int ONE_MINUTE_SECOND = 1000;
+    private static final int DEFAULT_START_TIME_MS = DEFAULT_START_TIME_MINUTE * ONE_SECOND;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +29,31 @@ public class TimerActivity extends AppCompatActivity {
         setContentView(R.layout.activity_timer);
 
         initToolbar();
+
+        TextView timerTextView = (TextView) findViewById(R.id.timer_text);
+        timerTextView.setText(getString(R.string.timer, DEFAULT_START_TIME_MINUTE, DEFAULT_START_TIME_SECOND));
+
+        CountDownTimer countDownTimer = new CountDownTimer(DEFAULT_START_TIME_MS, ONE_MINUTE_SECOND){
+
+            @Override
+            public void onFinish(){
+                Toast.makeText(getApplicationContext(), "洗濯終わったよ！", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onTick(long millisUntilFinished){
+                timerTextView.setText(getString(R.string.timer, millisUntilFinished/ONE_SECOND, millisUntilFinished/ONE_MINUTE_SECOND%60));
+            }
+        };
+
+        Button startButton = (Button) findViewById(R.id.start_button);
+        startButton.setOnClickListener(view -> countDownTimer.start());
+
+        Button cancelButton = (Button) findViewById(R.id.cancel_button);
+        cancelButton.setOnClickListener(view -> {
+            countDownTimer.cancel();
+            timerTextView.setText(getString(R.string.timer, DEFAULT_START_TIME_MINUTE, DEFAULT_START_TIME_SECOND));
+        });
     }
 
     private void initToolbar() {
